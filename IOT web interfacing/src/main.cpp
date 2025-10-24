b65f36edc6eaeb81f4da708623a862d589fa03bc
Abir-49 <talukdar2107049@stud.kuet.ac.bd>
2025-10-25 03:02:14 +0600

    project complete

--- IOT web interfacing/src/main.cpp
+++ IOT web interfacing/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#define DHTPIN 15
+#define DHTTYPE DHT22
+#define LEDPIN 2
+#define BLYNK_TEMPLATE_ID "TMPL68Oc4qv4Z"
+#define BLYNK_TEMPLATE_NAME "DHT22Assignment"
+#define BLYNK_AUTH_TOKEN "kmUnhEKYKIdCw7UPh54wE4gcntoLSwUy"
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+char ssid[] = "Wokwi-GUEST";
+char pass[] = "";
+
+DHT dht(DHTPIN, DHTTYPE);
+
+BlynkTimer timer;
+
+void sendSensorData() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (!isnan(temp)) {
+    Blynk.virtualWrite(V1, temp);
+  }
+
+  if (!isnan(hum)) {
+    Blynk.virtualWrite(V0, hum);
+  }
+}
+
+
+BLYNK_WRITE(V2) {
+  int ledState = param.asInt();
+  digitalWrite(LEDPIN, ledState);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LEDPIN, OUTPUT);
+  dht.begin();
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(2000L, sendSensorData);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
